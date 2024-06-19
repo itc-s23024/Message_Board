2d6b524ad31cb928c348bb0d57cbed11b307bb0b
itc-s23024 <s23024@std.it-college.ac.jp>
2024-06-19 15:02:33 +0900

    6/19

---
+++ src/main/kotlin/jp/ac/it_college/std/s23024/message_board/MessageBoardApplication.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.std.s23024.message_board
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MessageBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<MessageBoardApplication>(*args)
+}
